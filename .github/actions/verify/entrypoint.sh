a73ca96df447d7c73a978ebb458c2b1ff460295d
Vitor Sasaki <63128590+vSasakiv@users.noreply.github.com>
2024-08-11 20:28:49 -0300

    Update entrypoint.sh

--- .github/actions/verify/entrypoint.sh
+++ .github/actions/verify/entrypoint.sh
@@ -1,3 +1,20 @@
-#!/bin/sh -l
+#!/bin/bash
 
-ls /github/workspace
+touch project.tcl
+echo "load_package flow" >> project.tcl
+echo "project_new project -overwrite" >> project.tcl
+
+INFILE=/github/workspace/project.config
+PINFILE=/github/workspace/project.pin
+
+while read -r LINE
+do
+        echo "set_global_assignment -name $LINE" >> project.tcl
+done < "$INFILE"
+
+while read -r LINE
+do
+        MODULE_PIN=$(echo $LINE | cut -d'=' -f 1)
+        BOARD_PIN=$(echo $LINE | cut -d'=' -f 2)
+        echo "set_location_assignment $BOARD_PIN -to $MODULE_PIN" >> project.tcl
+done < "$PINFILE"

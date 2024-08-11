985bb8485102f987da46ef3feeb7150ed422e34f
Vitor Sasaki <63128590+vSasakiv@users.noreply.github.com>
2024-08-11 20:48:07 -0300

    Update entrypoint.sh

--- .github/actions/verify/entrypoint.sh
+++ .github/actions/verify/entrypoint.sh
@@ -12,9 +12,21 @@ do
         echo "set_global_assignment -name $LINE" >> project.tcl
 done < "$INFILE"
 
+VERILOG_LIST="$(find /github/workspace/hdl -name '*.v')"
+for VERILOG_FILE in $VERILOG_LIST;
+do echo "set_global_assignment --name VERILOG_FILE $VERILOG_FILE" >> project.tcl
+done
+
+TEXT_LIST="$(find /github/workspace/hdl -name '*.txt')"
+for TEXT_FILE in $TEXT_LIST;
+do echo "set_global_assignment --name TEXT_FILE $TEST_FILE" >> project.tcl
+done
+
 while read -r LINE
 do
         MODULE_PIN=$(echo $LINE | cut -d'=' -f 1)
         BOARD_PIN=$(echo $LINE | cut -d'=' -f 2)
         echo "set_location_assignment $BOARD_PIN -to $MODULE_PIN" >> project.tcl
 done < "$PINFILE"
+
+echo "project close" >> project.tcl
